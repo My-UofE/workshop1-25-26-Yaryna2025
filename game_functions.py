3115f62e9111c09760358ec753f9532f1176a17a
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-01-19 10:20:44 +0000

    improved function in game2

--- game_functions.py
+++ game_functions.py
@@ -7,10 +7,10 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    if current_val > next_val and user_input == 'l':
-        return True
-    elif current_val < next_val and user_input == 'h':
-        return True
+    if user_input == 'l':
+        return current_val > next_val
+    elif user_input == 'h':
+        return current_val < next_val
     else:
         False
 
